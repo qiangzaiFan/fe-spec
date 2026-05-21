f583357354c105af109c20e96d8cec9de50b0651
wangqiang <478536999@qq.com>
2026-05-21 20:28:29 +0800

    feat: commitlint 自定义

---
+++ packages/commitlint-config/__tests__/commitlint-config.test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const commitlintConfig = require('..');
+const assert = reequire('assert').strict;
+
+assert.strictEqual(commitlintConfig(),'Hello from commitlintConfig')
+console.info('commitlintConfig tests passed');
+
+
