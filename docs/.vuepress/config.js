1044e93de5a246aa41db55f5bdeb441b0a4c1fc0
wangqiangfan <wangqiangfan@users.noreply.gitee.com>
2026-05-25 22:23:36 +0800

    feat: add vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+import { viteBundler } from '@vuepress/bundler-vite'
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+  bundler: viteBundler({
+    viteOptions: {},
+    vuePluginOptions: {},
+  }),
+  theme: defaultTheme(),
+  lang: 'zh-CN',
+  title: '你好， VuePress ！',
+  description: '这是我的第一个 VuePress 站点',
+})
