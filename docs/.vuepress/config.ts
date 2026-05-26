670638900e9c64c4a60990b15d69bc003313da80
wangqiang <478536999@qq.com>
2026-05-26 10:53:25 +0800

    fix: deploy修改

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,20 +1,34 @@
-import { defineConfig4CustomTheme, UserPlugins } from 'vuepress/config';
+import { viteBundler } from '@vuepress/bundler-vite';
+import { defaultTheme } from '@vuepress/theme-default';
+import { defineUserConfig } from 'vuepress';
 
-export default defineConfig4CustomTheme({
-  locales: {
-    '/': {
-      lang: 'zh-CN',
-      title: '印客学院',
-      description: '前端编码规范工程化',
-    },
-  },
+export default defineUserConfig({
+  lang: 'zh-CN',
+  title: '印客学院',
+  description: '前端编码规范工程化',
   base: '/fe-spec/',
-  themeConfig: {
-    nav: [
+  bundler: viteBundler(),
+  head: [
+    ['link', { rel: 'icon', href: '/img/logo.png' }],
+    ['meta', { name: 'keywords', content: '前端编码规范工程化' }],
+  ],
+  theme: defaultTheme({
+    logo: '/img/logo.png',
+    repo: 'encode-studio-fe/fe-spec',
+    docsDir: 'docs',
+    contributors: false,
+    lastUpdated: false,
+    themePlugins: {
+      copyCode: {
+        showInMobile: false,
+      },
+      mediumZoom: true,
+    },
+    navbar: [
       { text: '首页', link: '/index.md' },
       {
         text: '编码规范',
-        items: [
+        children: [
           { text: 'HTML 编码规范', link: '/coding/html.md' },
           { text: 'CSS 编码规范', link: '/coding/css.md' },
           { text: 'JavaScript 编码规范', link: '/coding/javascript.md' },
@@ -24,7 +38,7 @@ export default defineConfig4CustomTheme({
       },
       {
         text: '工程规范',
-        items: [
+        children: [
           { text: 'Git 规范', link: '/engineering/git.md' },
           { text: '文档规范', link: '/engineering/doc.md' },
           { text: 'CHANGELOG 规范', link: '/engineering/changelog.md' },
@@ -32,7 +46,7 @@ export default defineConfig4CustomTheme({
       },
       {
         text: 'NPM包',
-        items: [
+        children: [
           { text: 'eslint-config-encode', link: '/npm/eslint.md' },
           { text: 'stylelint-config-encode', link: '/npm/stylelint.md' },
           { text: 'commitlint-config-encode', link: '/npm/commitlint.md' },
@@ -42,116 +56,42 @@ export default defineConfig4CustomTheme({
       },
       {
         text: '脚手架',
-        items: [{ text: 'encode-fe-lint', link: '/cli/encode-fe-lint.md' }],
+        children: [{ text: 'encode-fe-lint', link: '/cli/encode-fe-lint.md' }],
       },
     ],
     sidebar: [
       {
-        title: '编码规范',
+        text: '编码规范',
         children: [
-          {
-            title: 'HTML 编码规范',
-            path: '/coding/html.md',
-          },
-          {
-            title: 'CSS 编码规范',
-            path: '/coding/css.md',
-          },
-          {
-            title: 'JavaScript 编码规范',
-            path: '/coding/javascript.md',
-          },
-          {
-            title: 'Typescript 编码规范',
-            path: '/coding/typescript.md',
-          },
-          {
-            title: 'Node 编码规范',
-            path: '/coding/node.md',
-          },
+          '/coding/html.md',
+          '/coding/css.md',
+          '/coding/javascript.md',
+          '/coding/typescript.md',
+          '/coding/node.md',
         ],
       },
       {
-        title: '工程规范',
+        text: '工程规范',
         children: [
-          {
-            title: 'Git 规范',
-            path: '/engineering/git.md',
-          },
-          {
-            title: '文档规范',
-            path: '/engineering/doc.md',
-          },
-          {
-            title: 'CHANGELOG 规范',
-            path: '/engineering/changelog.md',
-          },
+          '/engineering/git.md',
+          '/engineering/doc.md',
+          '/engineering/changelog.md',
         ],
       },
       {
-        title: 'NPM包',
+        text: 'NPM包',
         children: [
-          { title: 'eslint-config-encode', path: '/npm/eslint.md' },
-          { title: 'stylelint-config-encode', path: '/npm/stylelint.md' },
-          { title: 'commitlint-config-encode', path: '/npm/commitlint.md' },
-          { title: 'markdownlint-config-encode', path: '/npm/markdownlint.md' },
-          { title: 'eslint-plugin-encode', path: '/npm/eslint-plugin.md' },
+          '/npm/eslint.md',
+          '/npm/stylelint.md',
+          '/npm/commitlint.md',
+          '/npm/markdownlint.md',
+          '/npm/eslint-plugin.md',
         ],
       },
       {
-        title: '脚手架',
-        children: [{ title: 'encode-fe-lint', path: '/cli/encode-fe-lint.md' }],
+        text: '脚手架',
+        children: ['/cli/encode-fe-lint.md'],
       },
     ],
-    logo: '/img/logo.png',
-    repo: 'encode-studio-fe/fe-spec',
-    searchMaxSuggestions: 10,
-    docsDir: 'docs',
-    footer: {
-      createYear: 2023,
-      copyrightInfo:
-        'encode studio | <a href="https://github.com/encode-studio-fe-web/fe-spec" target="_blank">github</a>',
-    },
-
-    extendFrontmatter: {
-      author: {
-        name: '澄怀',
-        link: 'https://github.com/encode-studio-fe-web/fe-sepc',
-      },
-    },
-  },
-
-  head: [
-    ['link', { rel: 'icon', href: '/img/logo.png' }],
-    [
-      'meta',
-      {
-        name: 'keywords',
-        content: '前端编码规范工程化',
-      },
-    ],
-  ],
-
-  plugins: <UserPlugins>[
-    [
-      'one-click-copy',
-      {
-        copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'],
-        copyMessage: '复制成功',
-        duration: 1000,
-        showInMobile: false,
-      },
-    ],
-
-    [
-      'vuepress-plugin-zooming',
-      {
-        selector: '.theme-vdoing-content img:not(.no-zoom)',
-        options: {
-          bgColor: 'rgba(0,0,0,0.6)',
-        },
-      },
-    ],
-  ],
-  extraWatchFiles: ['.vuepress/config.ts'],
+  }),
 });
