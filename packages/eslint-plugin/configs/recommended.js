1fd7f8b3ae10afe912e3f217fa04efc5b2a712e6
wangqiang <478536999@qq.com>
2026-06-08 15:20:50 +0800

    feat: add eslint-plugin

---
+++ packages/eslint-plugin/configs/recommended.js
@@ -0,0 +1,7 @@
+module.exports = {
+    plugins:['eslint-plugin-qz'],
+    rules:{
+        'eslint-plugin-qz/no-http-url':'warn',
+        'eslint-plugin-qz/no-secret-info':'error',
+    }
+}
---
+++ packages/eslint-plugin/rules/no-http-url.js
@@ -0,0 +1,12 @@
+const RULE_NAME = 'no-http-url' 
+module.exports = {
+    name:RULE_NAME,
+	meta: {
+		// TODO: add metadata
+	},
+	create(context) {
+		return {
+			// TODO: add callback function(s)
+		};
+	},
+};
